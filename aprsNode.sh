5ed11ffe0cb159efacffeada592f5f5f20105275
w4amRadio <104035184+w4amRadio@users.noreply.github.com>
2022-05-10 17:48:54 -0700

    Update aprsNode.sh

--- aprsNode.sh
+++ aprsNode.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #To be used with RPi-3 or higher
 
-sudo apt-get install -y ser2net
+apt-get install -y ser2net
 
-sudo apt-get install -y gpsd gpsd-clients
+apt-get install -y gpsd gpsd-clients
 
-sudo apt-get install -y axtools
+apt-get install -y axtools
 
-sudo apt-get install -y direwolf
+apt-get install -y direwolf
+addgroup direwolf audio
 
 #set up direwolf to be run as a service, move direwolf.conf to expected location and alter config
 
@@ -15,4 +16,4 @@ apt-get install -y xastir
 
 #user should typically be pi
 user=$(whoami)
-sudo usermod -a -G xastir-ax25 $user
+usermod -a -G xastir-ax25 $user
