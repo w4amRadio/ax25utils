03c66b03bff9d9373122b28aca8e22702d3af7f2
w4amRadio <104035184+w4amRadio@users.noreply.github.com>
2022-05-10 18:03:23 -0700

    Update aprsNode.sh

--- aprsNode.sh
+++ aprsNode.sh
@@ -18,6 +18,9 @@ echo "ax0 ${callsign}-1 1200  255		7		2m packet radio" >> /etc/ax25/axports
 apt-get install -y direwolf
 addgroup direwolf audio
 
+#at this point the signalink device should be plugged in
+arecord -l | grep "CODEC [USB AUDIO  CODEC]"
+
 #set up direwolf to be run as a service, move direwolf.conf to expected location and alter config
 
 apt-get install -y xastir
