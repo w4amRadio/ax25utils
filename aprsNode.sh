080fd1abbd48df5918ffe95863d4cce5cdf6187b
w4amRadio <104035184+w4amRadio@users.noreply.github.com>
2022-05-10 20:06:08 -0700

    Update aprsNode.sh

--- aprsNode.sh
+++ aprsNode.sh
@@ -6,27 +6,54 @@
 callsign=$1
 assignment=$2
 
+mkdir ~/tmp
+
+echo "Installing curl..." > ~/tmp/installation.log 2>&1
 apt-get install -y curl
+
+echo "Installing git..." >> ~/tmp/installation.log 2>&1
 apt-get install -y git
+
+echo "Installing gcc..." >> ~/tmp/installation.log 2>&1
 apt-get install -y gcc
+
+echo "Installing g++..." >> ~/tmp/installation.log 2>&1
 apt-get install -y g++
+
+echo "Installing make..." >> ~/tmp/installation.log 2>&1
 apt-get install -y make
+
+echo "Installing cmake..." >> ~/tmp/installation.log 2>&1
 apt-get install -y cmake
+
+echo "Installing libasound2-dev..." >> ~/tmp/installation.log 2>&1
 apt-get install -y libasound2-dev
+
+echo "Installing libudev-dev..." >> ~/tmp/installation.log 2>&1
 apt-get install -y libudev-dev
 
+echo "Installing ser2net..." >> ~/tmp/installation.log 2>&1
 apt-get install -y ser2net
 
+echo "Installing gpsd and gpsd-clients..." >> ~/tmp/installation.log 2>&1
 apt-get install -y gpsd gpsd-clients
 
+echo "Installing libax25..." >> ~/tmp/installation.log 2>&1
 apt-get install -y libax25
+
+echo "Installing ax25-apps..." >> ~/tmp/installation.log 2>&1
 apt-get install -y ax25-apps
+
+echo "Installing ax25-tools..." >> ~/tmp/installation.log 2>&1
 apt-get install -y ax25-tools
+
+echo "Installing axtools..." >> ~/tmp/installation.log 2>&1
 apt-get install -y axtools
 
+echo "Configuring /etc/ax25/axports..." >> ~/tmp/installation.log 2>&1 
 echo "ax0 ${callsign}-1 1200  255		7		2m packet radio" >> /etc/ax25/axports 
 
-
+echo "Installing direwolf...." >> ~/tmp/installation.log 2>&1
 git clone https://www.github.com/wb2osz/direwolf
 mkdir ~/direwolf/build
 cmake ~/direwolf
@@ -34,6 +61,7 @@ make ~/direwolf/build
 make install ~/direwolf/build
 make install-conf ~/direwolf/build
 
+echo "Adding direwolf to group audio..." >> ~/tmp/installation.log 2>&1
 addgroup direwolf audio
 
 #at this point the signalink device should be plugged in
