cc8d6b86fd74683e6118ad8225ccf44a8f71f440
w4amRadio <104035184+w4amRadio@users.noreply.github.com>
2022-05-19 00:15:53 -0400

    Update aprsNode.sh

--- aprsNode.sh
+++ aprsNode.sh
@@ -6,62 +6,61 @@
 callsign=$1
 assignment=$2
 
-mkdir ~/tmp
+user=$(logname)
 
-echo "Installing curl..." > ~/tmp/installation.log 2>&1
+mkdir /home/$user/tmp
+
+echo "Installing curl..." > /home/$user/tmp/installation.log 2>&1
 apt-get install -y curl
 
-echo "Installing git..." >> ~/tmp/installation.log 2>&1
+echo "Installing git..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y git
 
-echo "Installing gcc..." >> ~/tmp/installation.log 2>&1
+echo "Installing gcc..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y gcc
 
-echo "Installing g++..." >> ~/tmp/installation.log 2>&1
+echo "Installing g++..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y g++
 
-echo "Installing make..." >> ~/tmp/installation.log 2>&1
+echo "Installing make..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y make
 
-echo "Installing cmake..." >> ~/tmp/installation.log 2>&1
+echo "Installing cmake..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y cmake
 
-echo "Installing libasound2-dev..." >> ~/tmp/installation.log 2>&1
+echo "Installing libasound2-dev..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y libasound2-dev
 
-echo "Installing libudev-dev..." >> ~/tmp/installation.log 2>&1
+echo "Installing libudev-dev..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y libudev-dev
 
-echo "Installing ser2net..." >> ~/tmp/installation.log 2>&1
+echo "Installing ser2net..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y ser2net
 
-echo "Installing gpsd and gpsd-clients..." >> ~/tmp/installation.log 2>&1
+echo "Installing gpsd and gpsd-clients..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y gpsd gpsd-clients
 
-echo "Installing libax25..." >> ~/tmp/installation.log 2>&1
+echo "Installing libax25..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y libax25
 
-echo "Installing ax25-apps..." >> ~/tmp/installation.log 2>&1
+echo "Installing ax25-apps..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y ax25-apps
 
-echo "Installing ax25-tools..." >> ~/tmp/installation.log 2>&1
+echo "Installing ax25-tools..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y ax25-tools
 
-echo "Installing axtools..." >> ~/tmp/installation.log 2>&1
-apt-get install -y axtools
-
-echo "Configuring /etc/ax25/axports..." >> ~/tmp/installation.log 2>&1 
+echo "Configuring /etc/ax25/axports..." >> /home/$user/tmp/installation.log 2>&1 
 echo "ax0 ${callsign}-1 1200  255		7		2m packet radio" >> /etc/ax25/axports 
 
-echo "Installing direwolf...." >> ~/tmp/installation.log 2>&1
+echo "Installing direwolf...." >> /home/$user/tmp/installation.log 2>&1
 git clone https://www.github.com/wb2osz/direwolf
-mkdir ~/direwolf/build
-cmake ~/direwolf
-make ~/direwolf/build
-make install ~/direwolf/build
-make install-conf ~/direwolf/build
+mkdir /home/$user/direwolf/build
+cmake /home/$user/direwolf
+make /home/$user/direwolf/build
+make install /home/$user/direwolf/build
+make install-conf /home/$user/direwolf/build
 
-echo "Adding direwolf to group audio..." >> ~/tmp/installation.log 2>&1
+echo "Adding direwolf to group audio..." >> /home/$user/tmp/installation.log 2>&1
 addgroup direwolf audio
 
 #at this point the signalink device should be plugged in
@@ -71,10 +70,10 @@ device = $(arecord -l | grep "CODEC [USB AUDIO  CODEC]")
 searchResult=$(find / -name "direwolf.conf")
 
 #set up direwolf to be run as a service, move direwolf.conf to expected location and alter config
-echo "Configuring direwolf.conf..." >> ~/tmp/installation.log 2>&1
+echo "Configuring direwolf.conf..." >> /home/$user/tmp/installation.log 2>&1
 sed -i 's/YOURCALLSIGN/$(callsign)/g' direwolf.conf
 
-echo "Configuring direwolf service definition (/etc/systemd/system/direwolf.service) ..." >> ~/tmp/installation.log 2>&1
+echo "Configuring direwolf service definition (/etc/systemd/system/direwolf.service) ..." >> /home/$user/tmp/installation.log 2>&1
 echo "[Unit]
 Description=Direwolf TNC
 Requires=
@@ -90,18 +89,17 @@ LimitNOFILE=6000000
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/direwolf.service
 
-echo "Reloading Daemons..." >> ~/tmp/installation.log 2>&1
+echo "Reloading Daemons..." >> /home/$user/tmp/installation.log 2>&1
 systemctl daemon-reload
 
-echo "Enabling direwolf as a service..." >> ~/tmp/installation.log 2>&1
+echo "Enabling direwolf as a service..." >> /home/$user/tmp/installation.log 2>&1
 systemctl enable direwolf
 
-echo "Starting direwolf service..." >> ~/tmp/installation.log 2>&1
+echo "Starting direwolf service..." >> /home/$user/tmp/installation.log 2>&1
 systemctl start direwolf
 
-echo "Installing Xastir..." >> ~/tmp/installation.log 2>&1
+echo "Installing Xastir..." >> /home/$user/tmp/installation.log 2>&1
 apt-get install -y xastir
 
 #user should typically be pi
-user=$(whoami)
 usermod -a -G xastir-ax25 $user
